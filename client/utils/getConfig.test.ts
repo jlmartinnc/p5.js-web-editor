058d155c40c6b04da1cfc54606874e4f655c3f74
Claire Peng <clairepeng94@gmail.com>
2025-07-26 22:12:47 +0100

    update tests for get config after typing

--- client/utils/getConfig.test.ts
+++ client/utils/getConfig.test.ts
@@ -6,6 +6,14 @@ describe('utils/getConfig()', () => {
     delete window.process.env.CONFIG_TEST_KEY_NAME;
   });
 
+  // check for key
+  it('throws if key is empty string', () => {
+    expect(() => getConfig(/* key is empty string */ '')).toThrow(
+      /must be provided/
+    );
+  });
+
+  // check returns happy path
   it('fetches from global.process', () => {
     global.process.env.CONFIG_TEST_KEY_NAME = 'editor.p5js.org';
 
@@ -18,7 +26,19 @@ describe('utils/getConfig()', () => {
     expect(getConfig('CONFIG_TEST_KEY_NAME')).toBe('editor.p5js.org');
   });
 
+  // check returns unhappy path
   it('warns but does not throw if no value found', () => {
     expect(() => getConfig('CONFIG_TEST_KEY_NAME')).not.toThrow();
+  });
+
+  it('returns the expected nullish value when no value is found', () => {
+    const result = getConfig('CONFIG_TEST_KEY_NAME');
+    expect(result).toBe(undefined);
+    expect(!result).toBe(true);
+    expect(`${result}`).toBe('undefined');
+  });
+  it('can be set to return an empty string as the nullish value', () => {
+    const result = getConfig('CONFIG_TEST_KEY_NAME', { nullishString: true });
+    expect(`${result}`).toBe('');
   });
 });
