197fd67e1547082533b429be4d9f5264fb9e369a
Claire Peng <clairepeng94@gmail.com>
2025-08-03 12:38:45 +0100

    IconButton: migrate to typescript & add unit test

--- client/common/IconButton.tsx
+++ client/common/IconButton.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { ComponentType } from 'react';
 import styled from 'styled-components';
-import Button from './Button';
+import Button, { ButtonProps } from './Button';
 import { remSize } from '../theme';
 
 const ButtonWrapper = styled(Button)`
@@ -12,27 +11,21 @@ const ButtonWrapper = styled(Button)`
   }
 `;
 
-const IconButton = (props) => {
-  const { icon, ...otherProps } = props;
-  const Icon = icon;
-
-  return (
-    <ButtonWrapper
-      iconBefore={icon && <Icon />}
-      iconOnly
-      display={Button.displays.inline}
-      focusable="false"
-      {...otherProps}
-    />
-  );
+export type IconButtonProps = Omit<
+  ButtonProps,
+  'iconBefore' | 'display' | 'focusable'
+> & {
+  icon?: ComponentType<{ 'aria-label'?: string }> | null;
 };
 
-IconButton.propTypes = {
-  icon: PropTypes.func
-};
-
-IconButton.defaultProps = {
-  icon: null
-};
+const IconButton = ({ icon: Icon, ...otherProps }: IconButtonProps) => (
+  <ButtonWrapper
+    iconBefore={Icon ? <Icon /> : undefined}
+    iconOnly
+    display={Button.displays.inline}
+    focusable="false"
+    {...otherProps}
+  />
+);
 
 export default IconButton;
