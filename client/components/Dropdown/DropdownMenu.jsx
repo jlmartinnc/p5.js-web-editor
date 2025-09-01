160a9624e30ca50205471c954aedf27eb14de8dc
Claire Peng <clairepeng94@gmail.com>
2025-09-01 16:22:57 +0100

    Dropdown: Migrate DropdownWrapper to DropdownMenu and delete file due to none-use

--- client/components/Dropdown/DropdownMenu.jsx
+++ client/components/Dropdown/DropdownMenu.jsx
@@ -1,10 +1,76 @@
 import PropTypes from 'prop-types';
 import React, { forwardRef, useCallback, useRef, useState } from 'react';
+import styled from 'styled-components';
+import { remSize, prop } from '../../theme';
 import { useModalClose } from '../../common/useModalClose';
 import DownArrowIcon from '../../images/down-filled-triangle.svg';
-import { DropdownWrapper } from '../Dropdown';
 
 // TODO: enable arrow keys to navigate options from list
+const DropdownWrapper = styled.ul`
+  background-color: ${prop('Modal.background')};
+  border: 1px solid ${prop('Modal.border')};
+  box-shadow: 0 0 18px 0 ${prop('shadowColor')};
+  color: ${prop('primaryTextColor')};
+
+  position: absolute;
+  right: ${(props) => (props.right ? 0 : 'initial')};
+  left: ${(props) => (props.left ? 0 : 'initial')};
+
+  ${(props) => props.align === 'right' && 'right: 0;'}
+  ${(props) => props.align === 'left' && 'left: 0;'}
+
+
+  text-align: left;
+  width: ${remSize(180)};
+  display: flex;
+  flex-direction: column;
+  height: auto;
+  z-index: 2;
+  border-radius: ${remSize(6)};
+
+  & li:first-child {
+    border-radius: ${remSize(5)} ${remSize(5)} 0 0;
+  }
+  & li:last-child {
+    border-radius: 0 0 ${remSize(5)} ${remSize(5)};
+  }
+
+  & li:hover {
+    background-color: ${prop('Button.primary.hover.background')};
+    color: ${prop('Button.primary.hover.foreground')};
+
+    * {
+      color: ${prop('Button.primary.hover.foreground')};
+    }
+  }
+
+  li {
+    height: ${remSize(36)};
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+
+    & button,
+    & button span,
+    & a {
+      padding: ${remSize(8)} ${remSize(16)};
+      font-size: ${remSize(12)};
+    }
+
+    * {
+      text-align: left;
+      justify-content: left;
+
+      color: ${prop('primaryTextColor')};
+      width: 100%;
+      justify-content: flex-start;
+    }
+
+    & button span {
+      padding: 0px;
+    }
+  }
+`;
 
 const DropdownMenu = forwardRef(
   (
