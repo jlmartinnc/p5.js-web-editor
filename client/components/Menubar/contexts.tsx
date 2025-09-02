ee606031fde2e0215f25a13512dee85e079240e7
Claire Peng <clairepeng94@gmail.com>
2025-09-02 23:25:49 +0100

    Menubar/contexts: update to make all handlers optional

--- client/components/Menubar/contexts.tsx
+++ client/components/Menubar/contexts.tsx
@@ -7,35 +7,26 @@ export const MenuOpenContext = createContext<string>('none');
 interface MenubarContextType {
   createMenuHandlers: (
     id: string
-  ) => {
+  ) => Partial<{
     onMouseOver: (e: React.MouseEvent) => void;
     onClick: (e: React.MouseEvent) => void;
     onBlur: (e: React.FocusEvent) => void;
     onFocus: (e: React.FocusEvent) => void;
-  };
+  }>;
   createMenuItemHandlers: (
     id: string
-  ) => {
+  ) => Partial<{
     onMouseUp: (e: React.MouseEvent) => void;
     onBlur: (e: React.FocusEvent) => void;
     onFocus: (e: React.FocusEvent) => void;
-  };
+  }>;
   toggleMenuOpen: (id: string) => void;
   hasFocus: boolean;
 }
 
 export const MenubarContext = createContext<MenubarContextType>({
-  createMenuHandlers: () => ({
-    onMouseOver: () => {},
-    onClick: () => {},
-    onBlur: () => {},
-    onFocus: () => {}
-  }),
-  createMenuItemHandlers: () => ({
-    onMouseUp: () => {},
-    onBlur: () => {},
-    onFocus: () => {}
-  }),
+  createMenuHandlers: () => ({}),
+  createMenuItemHandlers: () => ({}),
   toggleMenuOpen: () => {},
   hasFocus: false
 });
