7a61ed35d4b641db42b1678d4251b32b25fe849a
Claire Peng <clairepeng94@gmail.com>
2025-10-08 14:48:05 +0100

    server/routes/user.routes: organise routes into subdomains

--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -4,40 +4,71 @@ import { isAuthenticated } from '../utils/isAuthenticated';
 
 const router = Router();
 
+/**
+ * ===============
+ * SIGN UP
+ * ===============
+ */
+// POST /signup
 router.post('/signup', UserController.createUser);
-
+// GET /signup/duplicate_check
 router.get('/signup/duplicate_check', UserController.duplicateUserCheck);
+// GET /verify
+router.get('/verify', UserController.verifyEmail);
+// POST /verify/send
+router.post('/verify/send', UserController.emailVerificationInitiate);
 
-router.put('/preferences', isAuthenticated, UserController.updatePreferences);
-
-router.post('/reset-password', UserController.resetPasswordInitiate);
-
-router.get('/reset-password/:token', UserController.validateResetPasswordToken);
-
-router.post('/reset-password/:token', UserController.updatePassword);
-
-router.put('/account', isAuthenticated, UserController.updateSettings);
-
-router.put(
-  '/cookie-consent',
-  isAuthenticated,
-  UserController.updateCookieConsent
-);
-
+/**
+ * ===============
+ * API KEYS
+ * ===============
+ */
+// POST /account/api-keys
 router.post('/account/api-keys', isAuthenticated, UserController.createApiKey);
-
+// DELETE /account/api-keys/:keyId
 router.delete(
   '/account/api-keys/:keyId',
   isAuthenticated,
   UserController.removeApiKey
 );
 
-router.post('/verify/send', UserController.emailVerificationInitiate);
+/**
+ * ===============
+ * PASSWORD MANAGEMENT
+ * ===============
+ */
+// POST /reset-password
+router.post('/reset-password', UserController.resetPasswordInitiate);
+// GET /reset-password/:token
+router.get('/reset-password/:token', UserController.validateResetPasswordToken);
+// POST /reset-password/:token
+router.post('/reset-password/:token', UserController.updatePassword);
+// PUT /account (updating password)
+router.put('/account', isAuthenticated, UserController.updateSettings);
 
-router.get('/verify', UserController.verifyEmail);
-
+/**
+ * ===============
+ * 3RD PARTY AUTH MANAGEMENT
+ * ===============
+ */
+// DELETE /auth/github
 router.delete('/auth/github', UserController.unlinkGithub);
+// DELETE /auth/google
 router.delete('/auth/google', UserController.unlinkGoogle);
+
+/**
+ * ===============
+ * USER PREFERENCES
+ * ===============
+ */
+// PUT /cookie-consent
+router.put(
+  '/cookie-consent',
+  isAuthenticated,
+  UserController.updateCookieConsent
+);
+// PUT /preferences
+router.put('/preferences', isAuthenticated, UserController.updatePreferences);
 
 // eslint-disable-next-line import/no-default-export
 export default router;
