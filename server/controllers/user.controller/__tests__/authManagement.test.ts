2350199647e0dd18cce4ed1ee5a8dd0b2076a67d
Claire Peng <clairepeng94@gmail.com>
2025-10-09 09:32:01 +0100

    user controller: add 3rd party auth management tests

--- server/controllers/user.controller/__tests__/authManagement.test.ts
+++ server/controllers/user.controller/__tests__/authManagement.test.ts
@@ -3,6 +3,7 @@ import { Response as MockResponse } from 'jest-express/lib/response';
 import { NextFunction as MockNext } from 'jest-express/lib/next';
 import { User } from '../../../models/user';
 import { unlinkGithub, unlinkGoogle } from '../../user.controller';
+import { saveUser } from '../helpers';
 
 import { mailerService } from '../../../utils/mail';
 import { renderEmailConfirmation } from '../../../views/mail';
@@ -17,6 +18,9 @@ jest.mock('../../../views/mail', () => ({
   renderEmailConfirmation: jest
     .fn()
     .mockReturnValue({ to: 'test@example.com', subject: 'Confirm' })
+}));
+jest.mock('../helpers', () => ({
+  saveUser: jest.fn()
 }));
 
 describe('user.controller > 3rd party auth management', () => {
@@ -37,7 +41,7 @@ describe('user.controller > 3rd party auth management', () => {
   });
 
   describe('unlinkGithub', () => {
-    it('returns 404 if user is not found', async () => {
+    it('returns 404 if user is not logged in', async () => {
       await unlinkGithub(request, response);
 
       expect(response.status).toHaveBeenCalledWith(404);
@@ -45,6 +49,52 @@ describe('user.controller > 3rd party auth management', () => {
         success: false,
         message: 'You must be logged in to complete this action.'
       });
+    });
+    it('removes the users github & filters out github tokens if user is logged in', async () => {
+      const user = {
+        github: { id: '123', username: 'testuser' },
+        tokens: [
+          { kind: 'github', accessToken: 'abc' },
+          { kind: 'google', accessToken: 'xyz' }
+        ]
+      };
+
+      request.user = user;
+
+      await unlinkGithub(request, response);
+
+      expect(user.github).toBeUndefined();
+      expect(user.tokens).toEqual([{ kind: 'google', accessToken: 'xyz' }]);
+      expect(saveUser).toHaveBeenCalledWith(response, user);
+    });
+  });
+
+  describe('unlinkGoogle', () => {
+    it('returns 404 if user is not logged in', async () => {
+      await unlinkGoogle(request, response);
+
+      expect(response.status).toHaveBeenCalledWith(404);
+      expect(response.json).toHaveBeenCalledWith({
+        success: false,
+        message: 'You must be logged in to complete this action.'
+      });
+    });
+    it('removes the users google & filters out google tokens if user is logged in', async () => {
+      const user = {
+        google: { id: '123', username: 'testuser' },
+        tokens: [
+          { kind: 'github', accessToken: 'abc' },
+          { kind: 'google', accessToken: 'xyz' }
+        ]
+      };
+
+      request.user = user;
+
+      await unlinkGoogle(request, response);
+
+      expect(user.google).toBeUndefined();
+      expect(user.tokens).toEqual([{ kind: 'github', accessToken: 'abc' }]);
+      expect(saveUser).toHaveBeenCalledWith(response, user);
     });
   });
 });
