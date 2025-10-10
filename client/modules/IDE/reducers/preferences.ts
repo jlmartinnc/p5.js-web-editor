867ef829b3cde1e71b8b5fcddad552ae91c18799
Claire Peng <clairepeng94@gmail.com>
2025-10-10 13:49:56 -0400

    --amend

--- client/modules/IDE/reducers/preferences.ts
+++ client/modules/IDE/reducers/preferences.ts
@@ -14,19 +14,19 @@ export interface PreferencesState
 export type PreferencesAction =
   | { type: typeof ActionTypes.OPEN_PREFERENCES }
   | { type: typeof ActionTypes.SET_PREFERENCES_TAB; value: PreferencesState['tabIndex'] }
-  | { type: typeof ActionTypes.SET_FONT_SIZE; value: Preferences['fontSize'] }
-  | { type: typeof ActionTypes.SET_AUTOSAVE; value: Preferences['autosave'] }
-  | { type: typeof ActionTypes.SET_LINEWRAP; value: Preferences['linewrap'] }
-  | { type: typeof ActionTypes.SET_LINT_WARNING; value: Preferences['lintWarning'] }
-  | { type: typeof ActionTypes.SET_TEXT_OUTPUT; value: Preferences['textOutput'] }
-  | { type: typeof ActionTypes.SET_GRID_OUTPUT; value: Preferences['gridOutput'] }
+  | { type: typeof ActionTypes.SET_FONT_SIZE; value: PreferencesState['fontSize'] }
+  | { type: typeof ActionTypes.SET_AUTOSAVE; value: PreferencesState['autosave'] }
+  | { type: typeof ActionTypes.SET_LINEWRAP; value: PreferencesState['linewrap'] }
+  | { type: typeof ActionTypes.SET_LINT_WARNING; value: PreferencesState['lintWarning'] }
+  | { type: typeof ActionTypes.SET_TEXT_OUTPUT; value: PreferencesState['textOutput'] }
+  | { type: typeof ActionTypes.SET_GRID_OUTPUT; value: PreferencesState['gridOutput'] }
   | { type: typeof ActionTypes.SET_PREFERENCES; preferences: PreferencesState }
-  | { type: typeof ActionTypes.SET_THEME; value: Preferences['theme'] }
-  | { type: typeof ActionTypes.SET_AUTOREFRESH; value: Preferences['autorefresh'] }
-  | { type: typeof ActionTypes.SET_LINE_NUMBERS; value: Preferences['lineNumbers'] }
-  | { type: typeof ActionTypes.SET_LANGUAGE; language: Preferences['language'] }
-  | { type: typeof ActionTypes.SET_AUTOCLOSE_BRACKETS_QUOTES; value: Preferences['autocloseBracketsQuotes'] }
-  | { type: typeof ActionTypes.SET_AUTOCOMPLETE_HINTER; value: Preferences['autocompleteHinter'] };
+  | { type: typeof ActionTypes.SET_THEME; value: PreferencesState['theme'] }
+  | { type: typeof ActionTypes.SET_AUTOREFRESH; value: PreferencesState['autorefresh'] }
+  | { type: typeof ActionTypes.SET_LINE_NUMBERS; value: PreferencesState['lineNumbers'] }
+  | { type: typeof ActionTypes.SET_LANGUAGE; language: PreferencesState['language'] }
+  | { type: typeof ActionTypes.SET_AUTOCLOSE_BRACKETS_QUOTES; value: PreferencesState['autocloseBracketsQuotes'] }
+  | { type: typeof ActionTypes.SET_AUTOCOMPLETE_HINTER; value: PreferencesState['autocompleteHinter'] };
 
 export const initialState: PreferencesState = {
   tabIndex: 0,
