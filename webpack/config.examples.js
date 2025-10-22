5d8f4fd03ce9ef68b1357ae50d61cbf84da74866
Claire Peng <clairepeng94@gmail.com>
2025-10-22 14:46:10 +0100

    make regex more flexible for safety to include jsx,tsx files even on server scripts

--- webpack/config.examples.js
+++ webpack/config.examples.js
@@ -52,7 +52,7 @@ module.exports = [
     module: {
       rules: [
         {
-          test: /\.[jt]s$/,
+          test: /\.[jt]sx?$/,
           exclude: /node_modules/,
           loader: 'babel-loader',
           options: {
@@ -82,7 +82,7 @@ module.exports = [
     module: {
       rules: [
         {
-          test: /\.[jt]s$/,
+          test: /\.[jt]sx?$/,
           exclude: /node_modules/,
           loader: 'babel-loader',
           options: {
