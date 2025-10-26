3fe0dd31fb514dc485f615b620cc1b7ba5e1bcf2
Claire Peng <clairepeng94@gmail.com>
2025-10-26 22:27:59 +0000

    client/modules/Legal/pages/PrivacyPolicy: update to named export, no types needed

--- client/modules/Legal/pages/PrivacyPolicy.tsx
+++ client/modules/Legal/pages/PrivacyPolicy.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Legal } from './Legal';
 
-function PrivacyPolicy() {
+export function PrivacyPolicy() {
   const { t } = useTranslation();
 
   return (
     <Legal policyFile="privacy-policy.md" title={t('Legal.PrivacyPolicy')} />
   );
 }
-
-export default PrivacyPolicy;
--- client/routes.jsx
+++ client/routes.jsx
@@ -8,7 +8,7 @@ import IDEView from './modules/IDE/pages/IDEView';
 import FullView from './modules/IDE/pages/FullView';
 import { About } from './modules/About/pages/About';
 import { CodeOfConduct } from './modules/Legal/pages/CodeOfConduct';
-import PrivacyPolicy from './modules/Legal/pages/PrivacyPolicy';
+import { PrivacyPolicy } from './modules/Legal/pages/PrivacyPolicy';
 import TermsOfUse from './modules/Legal/pages/TermsOfUse';
 import LoginView from './modules/User/pages/LoginView';
 import SignupView from './modules/User/pages/SignupView';
